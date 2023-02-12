0fdfdfb39c97017ab63fd3d96980a7f86671dfb9
PANKAJ SHARMA <gaur.ps21@outlook.com>
2023-02-12 09:54:15 +0000

    final

---
+++ src/test/java/com/microservices/SpringCloudProductServiceconfigclient/SpringCloudProductServiceconfigclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.SpringCloudProductServiceconfigclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudProductServiceconfigclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
